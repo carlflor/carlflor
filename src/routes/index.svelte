f51c3daea44b1e9172d120b5bed5a784d5262660
Carl Flor <carl.flor@grabtaxi.com>
2022-06-27 01:53:22 +0800

    add improvements

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,23 @@
+<script>
+    import { fade } from 'svelte/transition';
+    let visible = false;
+    setTimeout(() => { visible = true}, 500)
+</script>
 
-<section class="home">
-    <h1 class="greeting">🙋🏻‍♂️ Hi!</h1>
+<svelte:head>
+	<title>carlflor</title>
+	<meta name="description" content="I like helping people by writing code" />
+</svelte:head>
+
+{#if visible}
+<section transition:fade="{{ duration: 200 }}" class="home">
+    <h1 class="greeting">🙋🏻‍♂ Hi!</h1>
     <p class="bio">
-        I am Carl Flor, and I'm currently working as a senior software engineer at <a class="desc-link" href="https://www.grab.com/sg/" target="_blank">Grab</a> in Singapore. I like working on projects that make people's lives easier. Once in a while, I dedicate my weekends teaching coding.
-        When I am not coding, I am most likely taking <a class="desc-link" href="https://www.flickr.com/photos/132863000@N07/" target="_blank">pictures</a>, or <a class="desc-link" href="https://media.giphy.com/media/6fScAIQR0P0xW/giphy.gif" target="_blank">dancing</a>.
+        I am Carl Flor, and I am currently working as a senior software engineer at <a class="desc-link" href="https://www.grab.com/sg/" target="_blank">Grab</a> in Singapore. I like working on projects that make people's lives easier. Once in a while, I dedicate my weekends teaching coding.
+        When I am not coding, I am most likely taking <a class="desc-link" href="https://www.flickr.com/photos/132863000@N07/" target="_blank">pictures</a> or <a class="desc-link" href="https://media.giphy.com/media/6fScAIQR0P0xW/giphy.gif" target="_blank">dancing</a>.
     </p>
 </section>
+{/if}
 
 
 <style>
