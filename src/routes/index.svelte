35f0c31110e5e8c13874204d84ae6f363808eac8
Carl Flor <carl.flor@grabtaxi.com>
2022-12-10 15:13:25 +0800

    adding gif

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,8 +10,10 @@
     </p>
     <p class="bio">
         I currently work at <a class="desc-link" href="https://www.grab.com/sg/" target="_blank">Grab</a> where I build tools and systems that make customer support journey more pleasant. My favorite tool is Javascript, while I sometimes dabble with Golang and Ruby. Once in a while, I dedicate my weekends <a class="desc-link" href="https://www.linkedin.com/feed/update/urn:li:share:6884006384460009472?utm_source=linkedin_share&utm_medium=member_desktop_share&utm_content=post">teaching coding</a>.
-        When I am not coding, I am most likely taking <a class="desc-link" href="https://www.flickr.com/photos/132863000@N07/" target="_blank">pictures</a> or <a class="desc-link" href="https://media.giphy.com/media/6fScAIQR0P0xW/giphy.gif" target="_blank">dancing</a>.
+        When I am not coding, I am most likely taking <a class="desc-link" href="https://www.flickr.com/photos/132863000@N07/" target="_blank">pictures</a> or <span class="rainbow-text">dancing</span>.
+
     </p>
+    <img class="gif" alt="yoda-dancing-gif" src="https://i.pinimg.com/originals/bd/43/93/bd439311d5ddb4362a407413239b07d3.gif" />
     <p class="email">
         Feel free to drop me a message at <a class="desc-link" href="mailto:hello@carlflor.com" target="_top">hello@carlflor.com</a> for anything.
     </p>
@@ -32,5 +34,33 @@
 
     p + p {
         margin-top: 32px;
+    }
+
+    .rainbow-text {
+        background: linear-gradient( 92deg, #95d7e3, #eb76ff );
+        background: -webkit-linear-gradient( 92deg, #95d7e3, #eb76ff );
+        background-size:600vw 600vw;
+  
+        -webkit-background-clip: text;
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+        animation: textAnimate 0.5s linear infinite alternate;
+    }
+
+    @keyframes textAnimate {
+        from {
+            filter: hue-rotate(0deg);
+            background-position-x: 0%;
+            
+        }
+        to {
+            filter: hue-rotate(360deg);
+            background-position-x: 600vw;
+            
+        }
+    }
+
+    .gif {
+        width: 100%;
     }
 </style>
